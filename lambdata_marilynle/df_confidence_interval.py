fe5e0a9303db4bf5bc6a4e4b23fbe0d247f032e1
marilynle <marilynle@users.noreply.github.com>
2019-12-04 19:26:15 -0600

    df_confidence_interval.py updated

--- lambdata_marilynle/df_confidence_interval.py
+++ lambdata_marilynle/df_confidence_interval.py
@@ -1,18 +1,22 @@
-""
-The function calculates the confidence interval
-""
+
 
 from scipy.stats import sem, t
 from numpy import mean
 
-def confidence_interval(x,ci): # ci = 0.95
+class Confidence_interval:
+    ""
+    The function calculates the confidence interval
+    ""
+    def __init__(self, x, ci):
+        self.x = x
+        self.ci = ci  #  ci = 0.95
 
-  M = np.mean(x)                                   # Sample mean
-  ME = sem(x)* t.ppf((1 + ci) / 2, len(x) - 1)     # Margin of error
+    def MME(self):
+        self.mean = np.mean(self.x)  #  Sample mean
+        self.margin_error = sem(self.x)* t.ppf((1 + self.ci) / 2, len(self.x) - 1)  #  Margin of error
 
-  ci_endpoints = {
 
-          'The lower endpoint of the 95% CI is:': M - ME,
-          'The upper endpoint of the 95% CI is:': M + ME
-  }
-  return ci_endpoints
+    def ci_endpoints(self):
+        LE = self.mean - self.margin_error  #  lower endpoint of ci
+        UE = self.mean + self.margin_error  #  Upper endpoint of ci
+        return (LE,UE)
